4e97244c6eccafa29675416eb7d0e10bfca5e2e3
agathahelena <agatha.helena.so@gmail.com>
2025-04-01 11:55:32 -0300

    first commit

---
+++ exercicio01-template/src/App.vue
@@ -0,0 +1,68 @@
+<script setup>
+    const nome = 'agatha'
+    const idade = 15
+
+    function inverter(texto) {
+        return texto.split('').reverse().join('')
+    }
+
+    function saudacao() {
+        return `Olá, ${nome}!`
+    }
+
+    import {ref} from 'vue'
+
+const contador = ref(0);
+
+function incrementar(){
+  contador.value++
+}
+
+function decrementar(){
+  if(contador.value > 0){
+    contador.value--;
+  }
+}
+
+function reiniciar(){
+   contador.value = 0;
+}
+</script>
+
+<template>
+    <div class="info">
+        <h1>Exemplo 1 - Mostrando informações na tela</h1>
+        <p>Nome: {{ nome }} </p>
+        <p>Idade: {{ idade }}  anos</p>
+        <hr />
+        <p>Para completar 50 anos faltam: {{ 50 - idade }}  anos</p>
+        <p>O nome tem {{ nome.length }}  caracteres</p>
+        <p>O nome invertido é: {{ inverter(nome).toUpperCase() }} </p>
+        <hr />
+        <p> Exemplo de saudação usando função</p>
+        <p> {{ saudacao() }}  </p>
+    </div>
+    <div>
+      <h1>Contador</h1>
+    <p> {{ contador }} </p>
+    <button @click="incrementar">Incrementar</button>
+    <button @click="decrementar">Decrementar</button>
+    <button @click="reiniciar">Reiniciar</button>
+    </div>
+</template>
+
+<style scoped>
+.info {
+  background-color: rgba(35,12,22,1);
+  padding: 20px 30px;
+  color: rgb(183, 210, 219);
+  border-radius: 10px;
+}
+
+h1 {
+  color: white;
+  font-weight: bold;
+  font-size: 1.5em;
+}
+</style>
+
---
+++ exercicio1-template/src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+const nome = "joão";
+const idade = 25;
+
+function inverter(texto) {
+  return texto.split ('').reverse ().join('')
+}
+
+function saudacao (){
+  return 'Olá, ${nome}!' 
+}
+
+</script>
+
+<template>
+
+  <div class="info">
+    <h1>Exemplo 1 - mostrando informações na tela </h1>
+    <p> Nome: {{ nome }}</p>
+    <p>idade: {{ idade }}</p>
+    <hr/>
+    <p>Para completar 50 anos faltam: {{ 50-idade }} anos </p>
+    <p>O nome tem {{ nome.length }} caracteres</p>
+    <p>O nome invertido é : {{ inverter(nome) }}</p>
+    <hr/>
+    <p> Exemplo de saudação usando função</p>
+    <p>{{ saudacao ()}}</p>
+ </div>
+
+</template>
+
+<style scoped>
+.info {
+  background-color:rgba(35,12,22,1);
+  padding: 20px 30px;
+  color: rgb(183, 210, 219);
+  border-radius: 10px;
+  margin: 4vw;
+}
+
+h1{
+  color: white;
+  font-weight: bold;
+  font-size: 1.5em;
+}
+
+</style>
